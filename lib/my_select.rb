947d00c774f6de59758efbc97b9f6891200e9bcd
Lana Holmes <lana.lee.holmes@gmail.com>
2019-01-08 17:20:50 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -4,6 +4,14 @@ def my_select(collection)
 #    if yield(item) == true
 #      select_items << item
 #    end
+    puts item
+    puts yield(item)
     select_items << item if yield(item) == true
+    puts select_items
   end
+end
+
+nums = [1, 2, 3, 4]
+my_select(nums) do |num|
+  num.even?
 end
