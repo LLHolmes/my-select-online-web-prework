dd95ff1ea9cf7540f426629360e9fad2579ae43c
Lana Holmes <lana.lee.holmes@gmail.com>
2019-01-08 17:19:05 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -7,11 +7,3 @@ def my_select(collection)
     select_items << item if yield(item) == true
   end
 end
-
-def my_select(&block)
-  result =[]
-  self.each do |element|
-    result << element if block.call(element) == true
-  end
-  result
-end
